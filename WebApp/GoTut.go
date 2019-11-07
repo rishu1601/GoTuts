8557e93cc572109c7a0abd187f2166143fc88f5e
rishu1601 <rishukumar.com@gmail.com>
2019-11-07 19:51:52 +0530

    Added a struct, basics

--- WebApp/GoTut.go
+++ WebApp/GoTut.go
@@ -8,10 +8,26 @@ import (
 // There are no classes in go
 //Instead we use * structs *
 
+type car struct {
+	gasPedal      uint32
+	brakePedal    uint32
+	steeringWheel int32
+	topSpeed      float64
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Whoa! Writing through responseWriter")
 }
 func main() {
+	aCar := car{
+		gasPedal:      123,
+		brakePedal:    0,
+		steeringWheel: -10,
+		topSpeed:      200.1,
+	}
+	// aCar.attributeName would be used to get the data of attribute
+	fmt.Println(aCar)
+	fmt.Println(aCar.gasPedal)
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8000", nil)
 }
