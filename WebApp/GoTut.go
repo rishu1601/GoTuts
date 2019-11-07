aa32009787e9a04864b66f432abbd77bfaaf70ed
rishu1601 <rishukumar.com@gmail.com>
2019-11-07 22:48:08 +0530

    Basics Done

--- WebApp/GoTut.go
+++ WebApp/GoTut.go
@@ -6,12 +6,34 @@ import (
 	"net/http"
 )
 
+var stringBody string
+
 //Accessing the internet with Go
 
+//Get resp from a particular url
+func getResp(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	return resp, err
+}
+
+//Convert received response to string
+func convertByteToString(resp *http.Response) string {
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	stringBody := string(bytes)
+	return stringBody
+}
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, `<h1>Hi</h1>`)
+	fmt.Println(w, "Hi")
+}
+
 func main() {
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body) //Response is received in bytes
-	stringBody := string(bytes)           //Converting bytes to string
+	url := "https://www.washingtonpost.com/news-sitemaps/index.xml"
+	resp, _ := getResp(url)
+	stringBody := convertByteToString(resp)
 	fmt.Println(stringBody)
+	// http.HandleFunc("/", indexHandler)
+	// http.ListenAndServe(":8000", nil)
 	resp.Body.Close() //Close the resources used
+
 }
