3de222b0a552508e21a4954b581f8ea68e0a0885
rishu1601 <rishukumar.com@gmail.com>
2019-11-07 21:54:32 +0530

    Multi Line print

--- WebApp/GoTut.go
+++ WebApp/GoTut.go
@@ -5,51 +5,17 @@ import (
 	"net/http"
 )
 
-// There are no classes in go
-//Instead we use * structs *
-
-const usixteenmax float64 = 65535
-const kmConverter float64 = 1.60934
-
-type car struct {
-	gasPedal      uint16
-	brakePedal    uint16
-	steeringWheel int16
-	topSpeed      float64
-}
-
-func (c car) kmh() float64 {
-	return float64(c.gasPedal) * (c.topSpeed / usixteenmax)
-}
-func (c car) mph() float64 {
-	return float64(c.gasPedal) * (c.topSpeed / usixteenmax / kmConverter)
-}
-
-//Pointer receivers
-func (c *car) newTopSpeed(newSpeed float64) {
-	c.topSpeed = newSpeed
-}
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Whoa! Writing through responseWriter")
+	//Multi Line print
+	fmt.Fprintf(w, `<h1>Hi,where is this from?</h1>
+	<p>nvwe fewf ewf ewfewf</p>
+	<p>efef efw fw efefewfe</p>
+	`)
+	fmt.Fprintf(w, "<h1>Whoa!</h1> Writing through responseWriter")
+	fmt.Fprintf(w, "<p>But not this way</p>")
+	fmt.Fprintf(w, "<p>Its very static</p>")
 }
 func main() {
-	aCar := car{
-		gasPedal:      52345,
-		brakePedal:    0,
-		steeringWheel: -10,
-		topSpeed:      200.1,
-	}
-	// aCar.attributeName would be used to get the data of attribute
-	fmt.Println(aCar)
-	fmt.Println(aCar.gasPedal)
-	fmt.Println(aCar.kmh())
-	fmt.Println(aCar.mph())
-	//Changing top speed through pointer receiver
-	aCar.newTopSpeed(300.2)
-	//After changing top speed
-	fmt.Println("After changing top speed")
-	fmt.Println(aCar.kmh())
-	fmt.Println(aCar.mph())
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8000", nil)
 }
