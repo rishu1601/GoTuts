1e4af6565baffe25424e52d64287ce9841252fb2
rishu1601 <rishukumar.com@gmail.com>
2019-11-08 16:10:16 +0530

    Added synchronization

--- Concurrency.go
+++ Concurrency.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -10,26 +11,39 @@ func say(s string) {
 		fmt.Println(s)
 		time.Sleep(time.Millisecond * 100)
 	}
+	wg.Done() //Notifies that work has been done
 }
 
+// func main() {
+// 	//goroutine is like a lightweight thread
+// 	//to make a function concurrent,just add go infront of the function call
+
+// 	go say("Hi")
+// 	say("Why")
+
+// 	/*
+// 		goroutine is non blocking, so if program execution finishes before goroutine
+// 		we cannot have the functioncall completed
+// 	*/
+// 	//Try this
+
+// 	// go say("hi")
+// 	// go say("there")
+
+// 	//Try this
+
+// 	// say("hi")
+// 	// go say("why")
+// }
+
+//Goroutine synchronization
+
+var wg sync.WaitGroup
+
 func main() {
-	//goroutine is like a lightweight thread
-	//to make a function concurrent,just add go infront of the function call
-
-	go say("Hi")
-	say("Why")
-
-	/*
-		goroutine is non blocking, so if program execution finishes before goroutine
-		we cannot have the functioncall completed
-	*/
-	//Try this
-
-	// go say("hi")
-	// go say("there")
-
-	//Try this
-
-	// say("hi")
-	// go say("why")
+	wg.Add(1) //Adds to waitgroup
+	go say("hi")
+	wg.Add(1)
+	go say("no")
+	wg.Wait() //Asks for program to wait
 }
