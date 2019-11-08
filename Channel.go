9805170d1a5e2cd36387697db80f8fbf2805ce6f
rishu1601 <rishukumar.com@gmail.com>
2019-11-08 19:26:57 +0530

    Added buffer logic to channel

--- Channel.go
+++ Channel.go
@@ -1,34 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
 
 func foo(c chan int, someval int) {
+	defer wg.Done()
 	c <- someval * 5 //returns 5 * someval to the channel c
 }
 
 func main() {
-	c := make(chan int)
-	//send and receive of a channel is blocking
-	//So we dont need to synchronise these goroutines
-	// go foo(c, 3)
-	// go foo(c, 5)
-
-	// v1, v2 := <-c, <-c
-	// fmt.Println(v1, v2) //Prints 25,15 ... LIFO order
-
-	//Buffering and iterating over channels
+	c := make(chan int, 10) //10 indicates that we are buffering for 10 items
 	for i := 0; i < 10; i++ {
+		//Synchronise the waitgroup
+		wg.Add(1)
 		go foo(c, i)
 	}
+	wg.Wait()
+	//Wait for all goroutines to finish before closing the channels
+	close(c)
 	for item := range c {
 		fmt.Println(item)
-		/*
-			Gives the following error:
-			fatal error: all goroutines are asleep - deadlock!
-				goroutine 1 [chan receive]:
-
-			//This is because go routines dont comeback before the program ends
-			To get rid of this error we need to synchronise the goroutines
-		*/
 	}
 }
